5eced446b5d4f51b2bb5daeb5a303bd84c6ad641
viperovm <viperovm@gmail.com>
2024-06-11 21:59:44 +0300

    gallery backend

--- russian_auction_house/settings.py
+++ russian_auction_house/settings.py
@@ -190,6 +190,14 @@ CKEDITOR_5_CONFIGS = {
     'default': {
         'toolbar': ['heading', '|', 'bold', 'italic', 'link',
                     'bulletedList', 'numberedList', 'blockQuote', 'code', 'codeBlock', 'sourceEditing'],
+        'heading': {
+            'options': [
+                {'model': 'paragraph', 'title': 'Paragraph', 'class': 'ck-heading_paragraph'},
+                {'model': 'heading1', 'view': 'h1', 'title': 'Heading 1', 'class': 'ck-heading_heading1'},
+                {'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2'},
+                {'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3'}
+            ]
+        }
     },
     'extends': {
         'blockToolbar': [
