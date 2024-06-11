3bc0aaae31bfd733ce67947e13c368c9746077c0
viperovm <viperovm@gmail.com>
2024-06-11 21:50:57 +0300

    gallery backend

--- russian_auction_house/settings.py
+++ russian_auction_house/settings.py
@@ -189,7 +189,7 @@ customColorPalette = [
 CKEDITOR_5_CONFIGS = {
     'default': {
         'toolbar': ['heading', '|', 'bold', 'italic', 'link',
-                    'bulletedList', 'numberedList', 'blockQuote'],
+                    'bulletedList', 'numberedList', 'blockQuote', 'code'],
     },
     'extends': {
         'blockToolbar': [
