31aad3d4b4ce9b7ec42c49607b68fd8e94c24c0a
viperovm <viperovm@gmail.com>
2024-06-11 17:33:40 +0300

    add user

--- contacts/views.py
+++ contacts/views.py
@@ -7,24 +7,32 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-@api_view(['POST'])
+@api_view(['GET'])
 def obtain_email(request):
 
-    email = request.data.get('email')
-    payload = {
-        'format': 'json',
-        'api_key': '67jq9emuna394a5oxghmj9io1j9rkgzrurfoifna',
-        'list_ids': '3',
-        'fields[email]': email,
-        'double_optin': '3',
-        'overwrite': '0'
-    }
-    r = requests.get('https://api.unisender.com/ru/api/subscribe', params=payload)
+    email = request.GET.get('email', '')
 
-    if r.status_code == requests.codes.ok:
-        return Response(r.json(), status=status.HTTP_201_CREATED)
+    if email:
+        payload = {
+            'format': 'json',
+            'api_key': '67jq9emuna394a5oxghmj9io1j9rkgzrurfoifna',
+            'list_ids': '3',
+            'fields[email]': email,
+            'double_optin': '3',
+            'overwrite': '0'
+        }
+        r = requests.get('https://api.unisender.com/ru/api/subscribe', params=payload)
+        if r.status_code == requests.codes.ok:
+            return Response(r.json(), status=status.HTTP_201_CREATED)
+        else:
+            return Response(r.json(), status=status.HTTP_400_BAD_REQUEST)
     else:
-        return Response(r.json(), status=status.HTTP_400_BAD_REQUEST)
+        return Response('Ошибка передачи данных', status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
 
 
 
