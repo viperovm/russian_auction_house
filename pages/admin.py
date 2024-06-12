1bcd5acfa2212c8b78116f965b20834ea44c1aa4
viperovm <viperovm@gmail.com>
2024-06-12 12:44:40 +0300

    gallery backend

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Page
+from adminsortable2.admin import SortableAdminMixin
 
 
-admin.site.register(Page)
+class PageAdmin(SortableAdminMixin, admin.ModelAdmin):
+    list_display = ['name', 'url', 'description', 'is_active', 'my_order']
+
+
+admin.site.register(Page, PageAdmin)
 
--- pages/models.py
+++ pages/models.py
@@ -7,6 +7,7 @@ class Page(models.Model):
     url = models.CharField(max_length=80, verbose_name='url', null=True, blank=True)
     description = CKEditor5Field(verbose_name='Описание', config_name='default')
     is_active = models.BooleanField(verbose_name='Активная страница', default=True)
+    my_order = models.PositiveIntegerField(verbose_name='Сорт.', default=0, blank=False, null=False)
 
     def __str__(self):
         return self.name
@@ -14,4 +15,4 @@ class Page(models.Model):
     class Meta:
         verbose_name = 'Страница'
         verbose_name_plural = 'Страницы'
-        ordering = ['id']
+        ordering = ['my_order']
