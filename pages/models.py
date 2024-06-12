03b234b7a641b7ac81c5dd328f075390bed85aff
viperovm <viperovm@gmail.com>
2024-06-12 12:51:34 +0300

    gallery backend

--- pages/models.py
+++ pages/models.py
@@ -3,9 +3,9 @@ from django_ckeditor_5.fields import CKEditor5Field
 
 
 class Page(models.Model):
-    name = models.CharField(max_length=80, verbose_name='Название страницы', null=True, blank=True)
-    url = models.CharField(max_length=80, verbose_name='url', null=True, blank=True)
-    description = CKEditor5Field(verbose_name='Описание', config_name='default')
+    name = models.CharField(max_length=80, verbose_name='Название страницы', blank=False, null=False)
+    url = models.CharField(max_length=80, verbose_name='url', blank=False, null=False)
+    description = CKEditor5Field(verbose_name='Описание', config_name='default', null=True, blank=True)
     is_active = models.BooleanField(verbose_name='Активная страница', default=True)
     my_order = models.PositiveIntegerField(verbose_name='Сорт.', default=0, blank=False, null=False)
 
