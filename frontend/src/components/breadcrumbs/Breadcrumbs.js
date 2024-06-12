be9692eef3a789469ea2aaaf1f397915b9028657
viperovm <viperovm@gmail.com>
2024-06-12 14:08:09 +0300

    gallery backend

--- frontend/src/components/breadcrumbs/Breadcrumbs.js
+++ frontend/src/components/breadcrumbs/Breadcrumbs.js
@@ -7,13 +7,16 @@ const Breadcrumbs = ({items}) => {
       {items.length ? (
         <>
           {items.map((item, index) => (
-            <Link
-              key={index}
-              className={item.url ? "breadcrumbs_item_link" : "breadcrumbs_item_link--disabled"}
-              to={item.url}
-            >
-              {item.name}
-            </Link>
+            <>
+              <Link
+                key={index}
+                className={item.url ? "breadcrumbs_item_link" : "breadcrumbs_item_link--disabled"}
+                to={item.url}
+              >
+                {item.name}
+              </Link>
+              {item.url ? <div className="breadcrumbs_separator">/</div> : ""}
+            </>
           ))}
         </>
       ) : null}
