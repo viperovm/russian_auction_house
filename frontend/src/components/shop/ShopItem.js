e2508eea94ef7d86da0184ee845655de95cf108e
viperovm <viperovm@gmail.com>
2024-06-13 13:43:29 +0300

    gallery backend

--- frontend/src/components/shop/ShopItem.js
+++ frontend/src/components/shop/ShopItem.js
@@ -6,10 +6,7 @@ const ShopItem = ({data}) => {
     <div className="shop-item-description-wrapper">
       <div className="shop-item-description-artist">{data?.artist}</div>
       <div className="shop-item-description-name">{data?.name}</div>
-      <div className="shop-item-description">{data?.short_description}</div>
-      {/*<h2>{data?.artist}</h2>*/}
-      {/*<h1>{data?.name}</h1>*/}
-      {/*<p>{data?.short_description}</p>*/}
+      <div className="shop-item-short-description">{data?.short_description}</div>
       <div className="shop-item-price-wrapper">
         <div className="shop-item-price">{getProperPrice(data?.price)}</div>
         <button className="buy-button">купить</button>
