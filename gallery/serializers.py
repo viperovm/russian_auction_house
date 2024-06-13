42d235504bb8aeaf99a8466fae50eec63eed3aef
viperovm <viperovm@gmail.com>
2024-06-13 08:17:48 +0300

    gallery backend

--- gallery/serializers.py
+++ gallery/serializers.py
@@ -9,10 +9,7 @@ class PaintingImageSerializer(serializers.ModelSerializer):
 
 
 class PaintingSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(read_only=True)
-    price = serializers.CharField(read_only=True)
     painting_gallery = PaintingImageSerializer(read_only=True, many=True)
-    description = serializers.CharField(read_only=True)
 
     class Meta:
         model = Painting
