855bfacb30626fe1ef493b75db46bd3a2bed5798
viperovm <viperovm@gmail.com>
2024-06-13 16:41:01 +0300

    gallery backend

--- frontend/src/components/Inputs/Input.js
+++ frontend/src/components/Inputs/Input.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import required_img from '../../assets/img/required.svg'
-import { useMask } from '@react-input/mask';
+import {useMask} from '@react-input/mask';
 
 const Input = ({type, name, required, placeholder, label, data, handler}) => {
 
 
-  const inputRef = useMask({ mask: '+7 (___) ___-__-__', replacement: { _: /\d/ } });
+  const inputRef = useMask({mask: '+7 (___) ___-__-__', replacement: {_: /\d/}});
 
 
   return (
@@ -14,11 +14,16 @@ const Input = ({type, name, required, placeholder, label, data, handler}) => {
         {label}
         {required && <img src={required_img} className="input-required" alt="required"/>}
       </div>
-      {(type === 'text' || type === 'email') && <input name={name} type={type} placeholder={placeholder} value={data} onChange={e => handler(e)}/>}
-      {type === 'phone' && <div className="input-phone-wrapper">
-        <div className="input-phone-placeholder">+7 (___) ___-__-__</div>
-        <input className="input-phone" name={name} ref={inputRef} type="text" placeholder="+7 (___) ___-__-__" value={data} onChange={e => handler(e)}/>
-      </div>}
+      {(type === 'text' || type === 'email') &&
+      <input name={name} type={type} placeholder={placeholder} value={data} onChange={e => handler(e)}/>}
+      {/*{type === 'phone' && <div className="input-phone-wrapper">*/}
+      {/*  /!*<div className="input-phone-placeholder">+7 (___) ___-__-__</div>*!/*/}
+      {/*  <input className="input-phone" name={name} ref={inputRef} type="text" placeholder="+7 (___) ___-__-__"*/}
+      {/*         value={data} onChange={e => handler(e)}/>*/}
+      {/*</div>}*/}
+      {type === 'phone' &&
+      <input className="input-phone" name={name} ref={inputRef} type="text" placeholder="+7 (___) ___-__-__"
+             value={data} onChange={e => handler(e)}/>}
       {type === 'textarea' && <textarea name={name} value={data} onChange={e => handler(e)}/>}
     </div>
   );
