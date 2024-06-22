16f3ddfbbf863eeee5e8cd8a0885704e613578d0
viperovm <viperovm@gmail.com>
2024-06-23 02:39:57 +0300

    banner

--- frontend/src/components/banner/Banner.js
+++ frontend/src/components/banner/Banner.js
@@ -15,37 +15,24 @@ const Banner = () => {
     }
   }, [banner])
 
-  const targetRef = useRef();
-  const [width, setWidth] = useState(0);
-  const [mob, setMob] = useState(false);
-
-  useLayoutEffect(() => {
-    if (targetRef.current) {
-      setWidth(targetRef.current.offsetWidth);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (width < 992) {
-      setMob(true)
-    } else {
-      setMob(false)
-    }
-  }, [width])
-
   return (
-    <div ref={targetRef} className={`home-banner ${banner?.text_position}`}
-         style={{backgroundImage: `url(${banner?.image})`}}>
-      <div className="home-banner-info">
-        <div className="home-banner-title">{banner?.title}</div>
-        {banner?.subtitle && <div className="home-banner-subtitle">{banner?.subtitle}</div>}
-        {banner?.text && <div className="home-banner-text" dangerouslySetInnerHTML={{__html: nToBr(banner?.text)}}/>}
-        {banner?.link_1_url && <a href={banner?.link_1_url} className="home-banner-link" target='_blank'>{banner?.link_1_name}</a>}
-        {banner?.link_2_url && <a href={banner?.link_2_url} className="home-banner-link" target='_blank'>{banner?.link_2_name}</a>}
+    <>
+      {banner &&
+      <div className={`home-banner ${banner?.text_position}`}
+           style={{backgroundImage: `url(${banner?.image})`}}>
+        <div className="home-banner-info">
+          <div className="home-banner-title">{banner?.title}</div>
+          {banner?.subtitle && <div className="home-banner-subtitle">{banner?.subtitle}</div>}
+          {banner?.text && <div className="home-banner-text" dangerouslySetInnerHTML={{__html: nToBr(banner?.text)}}/>}
+          {banner?.link_1_url &&
+          <a href={banner?.link_1_url} className="home-banner-link" target='_blank'>{banner?.link_1_name}</a>}
+          {banner?.link_2_url &&
+          <a href={banner?.link_2_url} className="home-banner-link" target='_blank'>{banner?.link_2_name}</a>}
+        </div>
       </div>
-
-    </div>
-  );
-};
+      }
+    </>
+  )
+}
 
 export default Banner;
