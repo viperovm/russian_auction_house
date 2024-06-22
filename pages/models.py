5cda45040916eb1ac662ba9a432acb62bd4f86c4
viperovm <viperovm@gmail.com>
2024-06-22 14:58:33 +0300

    page meta

--- pages/models.py
+++ pages/models.py
@@ -3,6 +3,9 @@ from django_ckeditor_5.fields import CKEditor5Field
 
 
 class Page(models.Model):
+    meta_title = models.TextField(verbose_name='Title', null=True, blank=True)
+    meta_description = models.TextField(verbose_name='Description', null=True, blank=True)
+    meta_keywords = models.TextField(verbose_name='Keywords', null=True, blank=True)
     name = models.CharField(max_length=80, verbose_name='Название страницы', blank=False, null=False)
     slug = models.CharField(max_length=80, verbose_name='url', blank=False, null=False)
     description = CKEditor5Field(verbose_name='Описание', config_name='default', null=True, blank=True)
