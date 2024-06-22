0010183ed432f235c974f41364f499eda7f62531
viperovm <viperovm@gmail.com>
2024-06-23 02:00:36 +0300

    banner

--- frontend/src/components/banner/Banner.js
+++ frontend/src/components/banner/Banner.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useLayoutEffect, useRef, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {bannerAction} from "../../store/actions/siteActions";
+import {nToBr} from "../../functions/text";
 
 const Banner = () => {
 
@@ -36,13 +37,11 @@ const Banner = () => {
     <div ref={targetRef} className={`home-banner ${banner?.text_position}`}
          style={{backgroundImage: `url(${banner?.image})`}}>
       <div className="home-banner-info">
-
         <div className="home-banner-title">{banner?.title}</div>
         {banner?.subtitle && <div className="home-banner-subtitle">{banner?.subtitle}</div>}
-        {banner?.text && <div className="home-banner-text">{banner?.text}</div>}
+        {banner?.text && <div className="home-banner-text" dangerouslySetInnerHTML={{__html: nToBr(banner?.text)}}/>}
         {banner?.link_1_url && <a href={banner?.link_1_url} className="home-banner-link" target='_blank'>{banner?.link_1_name}</a>}
         {banner?.link_2_url && <a href={banner?.link_2_url} className="home-banner-link" target='_blank'>{banner?.link_2_name}</a>}
-
       </div>
 
     </div>
---
+++ frontend/src/functions/text.js
@@ -0,0 +1,3 @@
+export const nToBr = (text) => {
+  return text.replace('\n', '<br />')
+}
