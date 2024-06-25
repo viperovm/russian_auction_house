8a88ad022b9061b2c1f21a696e3c0e9c582a5dda
viperovm <viperovm@gmail.com>
2024-06-25 11:25:46 +0300

    auth front

--- stream/models.py
+++ stream/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 
 class Stream(models.Model):
+
+    CHOICES = (
+        ('vk', 'VK'),
+        ('youtube', 'YouTube'),
+    )
+
     name = models.CharField(max_length=80, verbose_name='Название стрима', blank=False, null=False)
+    source = models.CharField(verbose_name='Источник', choices=CHOICES, default='vk', blank=False)
     url = models.CharField(max_length=80, verbose_name='Ссылка', blank=False, null=False)
     is_active = models.BooleanField(verbose_name='Активный стрим', default=True)
 
